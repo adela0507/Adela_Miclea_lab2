aca03f1055d151e791f32984ad13d884a60a3dec
miclea adela <miclea adela@DESKTOP-5G56AP5>
2022-10-21 22:23:18 +0300

    Add project files.

---
+++ WebApplication1/Data/Adela_Miclea_lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Adela_Miclea_lab2.Models;
+
+namespace Adela_Miclea_lab2.Data
+{
+    public class Adela_Miclea_lab2Context : DbContext
+    {
+        public Adela_Miclea_lab2Context (DbContextOptions<Adela_Miclea_lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Adela_Miclea_lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Adela_Miclea_lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
